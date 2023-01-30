795eb8f45ec783d2cb8c2308c81036f75eab0c17
mohamad-nassar <mohamadnassar13.mn@gmail.com>
2023-01-30 13:46:34 +0200

    first commit

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cheerio = require('cheerio');
 const fs = require('fs');
 const app = express();
 app.use(express.json());
+app.use('/uploads',express.static(__dirname+"/uploads"));
 const port = 8000
 app.get('/*',(req,res)=>{return res.status(404).json({"msg":"Not Found!"})})
 app.post('/save-website', async(req, res) => {
@@ -11,7 +12,7 @@ app.post('/save-website', async(req, res) => {
         const { data: html } = await axios.get(req.body.websiteUrl);
         const $ = cheerio.load(html);
         const text = $('body').text();
-        fs.writeFileSync(`${new URL(req.body.websiteUrl).hostname.replace(/^www\./i, '').split('.')[0]}.txt`, text);
+        fs.writeFileSync(`./uploads/${new URL(req.body.websiteUrl).hostname.replace(/^www\./i, '').split('.')[0]}.txt`, text);
         res.json({ message: 'Website content saved successfully!' });
       } catch (err) {
         console.error(err);
